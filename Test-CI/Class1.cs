33163433b7f8f85506f06d3aea32413a730311ed
Sharma <kartik.e.sharma@accenture.com>
2018-10-22 20:23:31 +0530

    First commit

---
+++ Test-CI/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestCI
+{
+    class TestCI
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World.\n");
+        }
+    }
+}
---
+++ TestCI-MSTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestCI_MSTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
